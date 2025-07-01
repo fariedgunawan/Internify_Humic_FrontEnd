ed7af09da319cc71b7a51c533cf82c14fbee28bc
fariedgunawan <124080208+fariedgunawan@users.noreply.github.com>
2025-07-01 13:13:03 +0700

    update internship list

--- Internify_Project/src/Pages/InternshipList.tsx
+++ Internify_Project/src/Pages/InternshipList.tsx
@@ -112,7 +112,7 @@ const InternshipList = () => {
             <div className="flex gap-2">
               <input
                 type="text"
-                placeholder="Cari Nama, Jurusan"
+                placeholder="Cari Nama, Posisi"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
                 className="px-4 py-2 rounded-2xl w-[20rem] border border-slate-400"
